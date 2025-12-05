26cf6d787254d11baf0a1e29a3d899e484afae93
kusumasindri21 <kusumasindri21@gmail.com>
2025-12-05 10:05:42 +0700

    fix(fact_transaction): join stg_account to include customer_id

--- banking_dw/models/marts/fact/fact_transaction.sql
+++ banking_dw/models/marts/fact/fact_transaction.sql
@@ -1,8 +1,10 @@
--- fact_transaction.sql
 select
-    transaction_id,
-    account_id,
-    transaction_date,
-    amount,
-    transaction_type
-from {{ ref('stg_transaction') }}
+    t.transaction_id,
+    t.account_id,
+    a.customer_id,
+    t.transaction_date,
+    t.amount,
+    t.transaction_type
+from {{ ref('stg_transaction') }} as t
+left join {{ ref('stg_account') }} as a
+    on t.account_id = a.account_id
